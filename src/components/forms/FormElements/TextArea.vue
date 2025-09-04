a0655184f0e634318394d821db0311b0ecf520b6
Nguyễn Phúc Tĩnh <phuctinh2001@gmail.com>
2025-09-04 21:58:13 +0700

    QuillEditor

--- src/components/forms/FormElements/TextArea.vue
+++ src/components/forms/FormElements/TextArea.vue
@@ -5,12 +5,7 @@
       <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
         Description
       </label>
-      <textarea
-        v-model="normalDescription"
-        placeholder="Enter a description..."
-        rows="6"
-        class="dark:bg-dark-900 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
-      ></textarea>
+      <QuillEditor :style="'height:300px'" ref="quillRef" :modules="modules" toolbar="full" />
     </div>
 
     <!-- Disabled Textarea -->
@@ -18,13 +13,8 @@
       <label class="mb-1.5 block text-sm font-medium text-gray-300 dark:text-white/15">
         Description
       </label>
-      <textarea
-        v-model="disabledDescription"
-        placeholder="Enter a description..."
-        rows="6"
-        disabled
-        class="dark:bg-dark-900 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:shadow-focus-ring focus:outline-hidden focus:ring-0 disabled:border-gray-100 disabled:bg-gray-50 disabled:placeholder:text-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800 dark:disabled:border-gray-800 dark:disabled:bg-white/[0.03] dark:disabled:placeholder:text-white/15"
-      ></textarea>
+      <textarea v-model="disabledDescription" placeholder="Enter a description..." rows="6" disabled
+        class="dark:bg-dark-900 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:shadow-focus-ring focus:outline-hidden focus:ring-0 disabled:border-gray-100 disabled:bg-gray-50 disabled:placeholder:text-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800 dark:disabled:border-gray-800 dark:disabled:bg-white/[0.03] dark:disabled:placeholder:text-white/15"></textarea>
     </div>
 
     <!-- Error State Textarea -->
@@ -32,12 +22,8 @@
       <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
         Description
       </label>
-      <textarea
-        v-model="errorDescription"
-        placeholder="Enter a description..."
-        rows="6"
-        class="dark:bg-dark-900 w-full rounded-lg border border-error-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-error-300 focus:outline-hidden focus:ring-3 focus:ring-error-500/10 dark:border-error-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-error-800"
-      ></textarea>
+      <textarea v-model="errorDescription" placeholder="Enter a description..." rows="6"
+        class="dark:bg-dark-900 w-full rounded-lg border border-error-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-error-300 focus:outline-hidden focus:ring-3 focus:ring-error-500/10 dark:border-error-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-error-800"></textarea>
       <p class="mt-1.5 text-theme-xs text-error-500">Please enter a message in the textarea.</p>
     </div>
   </div>
@@ -45,8 +31,33 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import ImageUploader from 'quill-image-uploader';
+import { QuillEditor } from '@vueup/vue-quill'
 
 const normalDescription = ref('')
 const disabledDescription = ref('This textarea is disabled')
 const errorDescription = ref('')
+
+const modules = ref({
+  name: 'imageUploader',
+  module: ImageUploader,
+  options: {
+    upload: (file: File) => {
+      return new Promise(() => {
+        const formData = new FormData();
+        formData.append("image", file);
+
+        // axios.post('/upload-image', formData)
+        //   .then(res => {
+        //     console.log(res)
+        //     resolve(res.data.url);
+        //   })
+        //   .catch(err => {
+        //     reject("Upload failed");
+        //     console.error("Error:", err)
+        //   })
+      })
+    }
+  }
+})
 </script>
